2e65a3f84bc3c869d191ddba34d69846d5aa64d1
Sioweb <support@sioweb.de>
2018-05-21 13:54:54 +0200

    add efg support

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,7 +5,7 @@
  */
 
 /**
- * @file autoload.php
+ * @file config.php
  * @author Sascha Weidner
  * @package sioweb.dsgvo
  * @copyright Sioweb, Sascha Weidner
@@ -15,3 +15,5 @@ $GLOBALS['TL_FFL']['dsgvo'] = 'Sioweb\Dsgvo\Forms\FormDsgvo';
 
 $GLOBALS['TL_CSS'][] = 'web/bundles/siowebdsgvo/css/jquery.confirm.css|static';
 $GLOBALS['TL_JAVASCRIPT'][] = 'web/bundles/siowebdsgvo/js/jquery.confirm.js|static';
+
+$GLOBALS['EFG']['storable_fields'][] = 'dsgvo';
