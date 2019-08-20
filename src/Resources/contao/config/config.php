e71d3fb05cc76dd829da9a7f9eb5626c879217fa
Sascha Weidner <support@sioweb.de>
2019-08-20 10:29:50 +0200

    Use correct path for assets

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -13,9 +13,9 @@
 
 $GLOBALS['TL_FFL']['dsgvo'] = 'Sioweb\Dsgvo\Forms\FormDsgvo';
 
-$GLOBALS['TL_CSS'][] = 'web/bundles/siowebdsgvo/css/jquery.confirm.css|static';
-$GLOBALS['TL_JAVASCRIPT'][] = 'web/bundles/siowebdsgvo/js/jquery.confirm.js|static';
-$GLOBALS['TL_JAVASCRIPT'][] = 'web/bundles/siowebdsgvo/js/dsgvo.confirm.js|static';
-$GLOBALS['TL_JAVASCRIPT'][] = 'web/bundles/siowebdsgvo/js/loadDsgvo.js|static';
+$GLOBALS['TL_CSS'][] = 'bundles/siowebdsgvo/css/jquery.confirm.css|static';
+$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/siowebdsgvo/js/jquery.confirm.js|static';
+$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/siowebdsgvo/js/dsgvo.confirm.js|static';
+$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/siowebdsgvo/js/loadDsgvo.js|static';
 
 $GLOBALS['EFG']['storable_fields'][] = 'dsgvo';
